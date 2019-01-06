69f9074707ff3b6ec1e2d6ac1708fd03329abcbc
github-user-1002 <vsaryuv@gmail.com>
2019-01-06 17:39:23 +0530

    my first commit

---
+++ week_02/interrogation_by_principal.py
@@ -0,0 +1,15 @@
+name=input("What is your name,dear?\n")
+
+print("Hello "+name)
+
+grade=input("Which grade are you in?\n")
+
+print("Okay, "+name+" you are in grade "+grade)
+
+place=input("Where do you live, nana?\n")
+
+print("Oh! You live in "+place+", nana?\nBeautiful place.")
+
+print("Okay, "+name+". Bye nana!")
+
+
