e28d3add848371ff9972758e474e7170a8573adb
github-user-1002 <vsaryuv@gmail.com>
2019-01-13 16:20:07 +0530

    instructor commit

---
+++ week_03/variables_keywords.py
@@ -0,0 +1,33 @@
+print("We are in week 3")
+
+
+a = 6
+print(a)
+print(type(a))
+
+
+b = "6"
+print(b)
+print(type(b))
+
+
+c = True
+print(c)
+print(type(c))
+
+
+d = 0.0567
+print(d)
+print(type(d))
+
+
+
+i=3000
+print(i)
+print(float(i))
+
+dec = 4.675
+print(dec)
+
+i_dec = int(5.5)
+print(i_dec)
---
+++ week_04/conversio_of_data_types.py
@@ -0,0 +1,144 @@
+a = 3.4556
+print(a)
+print(type(a))
+print(int(a))
+print("integer")
+
+b = 5.99
+print(b)
+print(type(b))
+print(int(b))
+print("integer")
+
+c = 3.87
+print(c)
+print(type(c))
+print(str(c))
+print("string")
+
+d = 3629.88
+print(d)
+print(type(d))
+print(str(d))
+print("string")
+
+e = 35468.9
+print(e)
+print(type(e))
+print(bool(e))
+print("boolean")
+
+f = 746.23
+print(f)
+print(type(f))
+print(bool(f))
+print("boolean")
+
+g = 54
+print(g)
+print(type(g))
+print(float(g))
+print("float")
+
+h = 2437850
+print(h)
+print(type(h))
+print(float(h))
+print("float")
+
+i = 243400
+print(i)
+print(type(i))
+print(bool(i))
+print("boolean")
+
+j = 243400
+print(j)
+print(type(j))
+print(bool(j))
+print("boolean")
+
+k = 10944
+print(k)
+print(type(k))
+print(str(k))
+print("string")
+
+r = False
+print(r)
+print(type(r))
+print(str(r))
+print("string")
+
+l = True
+print(l)
+print(type(l))
+print(int(l))
+print("integer")
+
+m = False
+print(m)
+print(type(m))
+print(int(m))
+print("integer")
+
+n = True
+print(n)
+print(type(n))
+print(float(n))
+print("float")
+
+o = False
+print(o)
+print(type(o))
+print(float(o))
+print("float")
+
+p = True
+print(p)
+print(type(p))
+print(str(p))
+print("string")
+
+q = False
+print(n)
+print(type(n))
+print(str(n))
+print("string")
+
+s = happy
+print(s)
+print(type(s))
+print(float(s))
+print("float")
+
+t = sad
+print(t)
+print(type(t))
+print(float(t))
+print("float")
+
+u = surpise
+print(u)
+print(type(u))
+print(int(u))
+print("integer")
+
+v = dissapointed
+print(v)
+print(type(v))
+print(int(v))
+print("integer")
+
+w = maybe
+print(w)
+print(type(w))
+print(bool(w))
+print("boolean")
+
+x = boring
+print(x)
+print(type(x))
+print(bool(x))
+print("boolean")
+
