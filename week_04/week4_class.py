b4bb7e60838704942fec87a2122b2b1f4a76b494
github-user-1002 <vsaryuv@gmail.com>
2019-01-13 18:02:11 +0530

    week 4 commit

---
+++ week_04/week4_class.py
@@ -0,0 +1,59 @@
+a = 10
+b = 3
+c = a%b
+print(c)
+print("Modulus")
+
+d = 30
+e = 45
+g = d-e
+print(g)
+print("Subtraction")
+
+m = 2
+o = 3
+i = m**o
+print(i)
+print("Exponents")
+
+y = 88
+t = 8
+p = y+t
+print(p)
+print("Addition")
+
+r = 99
+q = 11
+l = r/q
+print(l)
+print("Division")
+
+mmu = 77
+ii = 7
+dd = mmu*ii
+print(dd)
+print("Multiplication")
+
+w = 23
+s = 45
+rt = w**s
+print(rt)
+# Calculation using variables and arithmetic operations
+
+print(20==4)
+print(2==2)
+print(20>4)
+print(20<4)
+print(20!=4)
+print(20<=4)
+print(20>=4)
+
+
+
+a = int(input("Please enter in the first no.\n"))
+b = int(input("Please enter your second no.\n"))
+
+if a<b :
+    print(str(a)+ " is greater than "+str(b))
+else :
+    print("Then, "+str(b)+" is greater than "+str(a)
